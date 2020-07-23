bff8f28d6b46f9b00bc7a8bd4dce2379ab44c68c
孤狼 <wolf@evolify.cn>
2020-07-23 14:28:13 +0800

    feat(dom): add funtion to dom utils
    affects: @evolify/dom-utils

--- packages/dom/src/index.ts
+++ packages/dom/src/index.ts
@@ -1,3 +1,12 @@
 export function className(...cls: Array<string | boolean | undefined | null>) {
   return cls.filter(Boolean).join(" ")
 }
+
+export function getCookie(name: string) {
+  let arr,
+    reg = new RegExp(`(^| )${name}=([^;]*)(;|$)`);
+  if (arr = document.cookie.match(reg)) {
+    return unescape(arr[2]);
+  }
+  return null;
+}
